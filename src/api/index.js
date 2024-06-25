1d7e3f80fcf319a84b9f0917b0ec2b8fa8186f22
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2024-06-25 23:41:43 +0300

    feat: added API folder

--- src/api/index.js
+++ src/api/index.js
@@ -1,37 +1,29 @@
-// import axios from "axios";
+import axios from 'axios';
 
-// const API_KEY = 'AIzaSyAo-wFx9JQiJ3NnZlaygtzcQjMmTp80F2Y'
-// const url = 'https://www.googleapis.com/books/v1/volumes'
+const getBooks = async form => {
+  const API_KEY = 'AIzaSyAL8zHF2VMT2bFP6z9euyvZhufsPUqHkGY';
+  const url = 'https://www.googleapis.com/books/v1/volumes';
 
-// const searchBooks = async (form) => {
-//     const query = `${form.title}${form.subject && form.subject !== 'all' ? `+subject:${form.subject}` : ''}`;
-//     try {
-//         const response = await axios.get(url, {
-//             params: {
-//                 q: query || 'all',
-//                 orderBy: form.sorting || 'relevance',
-//                 key: API_KEY,
-//                 maxResults: 40,
-//             },
-//         });
+  const response = await axios.get(url, {
+    params: {
+      q: form.title || 'all',
+      orderBy: form.sorting || 'relevance',
+      key: API_KEY,
+      maxResults: 40,
+    },
+  });
+  return response;
+};
 
-//         return response.data.items
-//     } catch (error) {
-//         console.error('Failed to fetch books from Google Books API:', error);
-//     }
-// };
+const getBookById = async bookId => {
+  const API_KEY = 'AIzaSyAL8zHF2VMT2bFP6z9euyvZhufsPUqHkGY';
+  const url = `https://www.googleapis.com/books/v1/volumes/${bookId}`;
 
-// const getBookDetails = async (bookId) => {
-//     try {
-//         const response = await axios.get(`${url}/${bookId}`, {
-//             params: {
-//                 key: API_KEY,
-//             },
-//         });
-//         setBook(response.data);
-//     } catch (error) {
-//         console.error('Failed to fetch book details from Google Books API:', error);
-//     }
-// };
-
-// export { getBookDetails, searchBooks }
+  const response = await axios.get(url, {
+    params: {
+      key: API_KEY,
+    },
+  });
+  return response;
+};
+export { getBooks, getBookById };
