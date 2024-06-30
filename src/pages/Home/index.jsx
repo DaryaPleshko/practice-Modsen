8b20e2809b8a4a61fad5a4ec3d63bc6e4398a11a
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2024-06-30 23:19:17 +0300

    feat: 'folder structure changed; ref context'

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Header } from '../../components/Header/Header';
+import { Header } from '../../components/Header';
 import { getBooks } from '../../api';
 import style from './style.module.scss';
 import { SearchBooksProvider } from '../../context/SearchBooksProvider';
